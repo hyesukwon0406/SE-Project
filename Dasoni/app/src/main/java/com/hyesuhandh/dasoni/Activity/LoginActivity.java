2ed5fa6df047f9171b5c1d1a2bbca45e02620b20
Jihyeon Bae <jhbjhjh22@gmail.com>
2022-05-15 01:07:17 +0900

    dasoni2

--- Dasoni/app/src/main/java/com/hyesuhandh/dasoni/Activity/LoginActivity.java
+++ Dasoni/app/src/main/java/com/hyesuhandh/dasoni/Activity/LoginActivity.java
@@ -3,8 +3,6 @@ package com.hyesuhandh.dasoni.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hyesuhandh.dasoni.databinding.ActivityLoginBinding;
@@ -17,17 +15,32 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        Button okbtn = binding.okbtn;
-
-        okbtn.setOnClickListener(new View.OnClickListener() {
+        View view = binding.getRoot();
+        setContentView(view);
+        //로그인 확인
+        binding.okbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(getApplicationContext(),UserConnectionActivity.class);
+                Intent intent = new Intent(LoginActivity.this, UserConnectionActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        //id/pw 찾기
+        binding.findbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, IdpwFindActivity.class);
                 startActivity(intent);
             }
         });
-
-
+        //회원가입
+        binding.joinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, JoinActiity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
