4077b026fe5ed341a6bf8f67eae3311e23cb5ca3
Jihyeon Bae <jhbjhjh22@gmail.com>
2022-05-17 00:18:27 +0900

    로그인,회원가입1

--- Dasoni/app/src/main/java/com/hyesuhandh/dasoni/Activity/JoinActiity.java
+++ Dasoni/app/src/main/java/com/hyesuhandh/dasoni/Activity/JoinActiity.java
@@ -1,18 +1,68 @@
 package com.hyesuhandh.dasoni.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.hyesuhandh.dasoni.databinding.ActivityJoinActiityBinding;
-
+import java.util.HashMap;
+import java.util.Map;
 public class JoinActiity extends AppCompatActivity {
     private ActivityJoinActiityBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
         binding = ActivityJoinActiityBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.joinokbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUp();
+            }
+        });
+    }
+    private void signUp(){
+        final String name = (binding.namefild).getText().toString().trim();
+        final String email = (binding.emailfild).getText().toString().trim();
+        final String password = (binding.pwlyfild).getText().toString().trim();
+        final String pwcheck = (binding.okpwlyfild).getText().toString().trim();
+        if(name.length()>0 &&password.length()>0&&pwcheck.length()>0&&email.length()>0){
+            if(password.equals(pwcheck)){
+                auth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(JoinActiity.this, new OnCompleteListener<AuthResult>(){
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(JoinActiity.this,LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    Toast.makeText(JoinActiity.this,"등록 에러", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                            }
+                        });
+            };
+
+        }
+
     }
 }
--- Dasoni/app/src/main/java/com/hyesuhandh/dasoni/Activity/LoginActivity.java
+++ Dasoni/app/src/main/java/com/hyesuhandh/dasoni/Activity/LoginActivity.java
@@ -3,12 +3,24 @@ package com.hyesuhandh.dasoni.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.hyesuhandh.dasoni.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +29,23 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        auth = FirebaseAuth.getInstance();
         //로그인 확인
         binding.okbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, UserConnectionActivity.class);
-                startActivity(intent);
+                if(binding.Idfild.length()>0){
+                    if(binding.Pwfild.length()>0){
+                        logIn();
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
+                    }
 
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         //id/pw 찾기
@@ -42,5 +64,23 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+    //로그인 기능
+    public void logIn(){
+        String email = binding.Idfild.getText().toString().trim();
+        String pwd = binding.Pwfild.getText().toString().trim();
+        auth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {//성공했을때
+                            Intent intent = new Intent(LoginActivity.this, UserConnectionActivity.class);
+                            startActivity(intent);
+                        } else {//실패했을때
+                            Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
